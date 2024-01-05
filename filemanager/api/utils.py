1fd7b7480d81633f318f8ad94038a409c6ceb817
chimchimster <jorddyishere@gmail.com>
2024-01-05 12:59:45 +0600

    start refactoring filters for HTTP requests

---
+++ filemanager/api/utils.py
@@ -0,0 +1,147 @@
+import abc
+from typing import Optional
+
+
+class HttpRequestFilter(type):
+
+    # Common filters intended to search accurate values using Django ORM.
+    # Example of usage:
+    #       CustomModel.objects.filter(**{'field': value})
+    #       or in more common way CustomModel.objects.filter(field=value)
+    __common_filter_names = (
+        'file_extension',
+        'file_uuid',
+        'status',
+    )
+
+    # Timed filters is intended to filter records by the timestamps.
+    # Example of usage:
+    #       CustomModel.objects.filter(**{'field_gte', value}). Choices are gte, lte.
+    # Note that If "start" equals "end" means that eq is used.
+    __timed_filter_names = (
+        'start',
+        'end',
+    )
+
+    # Pagination filters is intended to control number of records displayed on each page.
+    # Example of usage:
+    #       CustomModel.objects.filter(field=value)[start_index:end_index]
+    __pagination_filter_names = (
+        'page',
+        'page_size',
+    )
+
+    def __new__(cls, name, bases, namespace):
+
+        print(namespace)
+        print()
+        available_request_params = namespace.get('_%s__available_request_params' % namespace.get('__qualname__', ''), ())
+        print(available_request_params)
+        for request_param in available_request_params:
+            print(request_param)
+            filter_name = f'{request_param.lower()}_filters'
+
+            namespace[filter_name] = cls.create_filter_method(request_param)
+
+        return super().__new__(cls, name, bases, namespace)
+
+    @staticmethod
+    def create_filter_method(request_param):
+        def filter_method(self, request):
+
+            nonlocal request_param
+
+            if request_param == 'GET':
+
+                cm_flt = HttpRequestFilter.__get_common_filters(self, request)
+                tm_flt = HttpRequestFilter.__get_timed_filters(self, request)
+                pg_flt = HttpRequestFilter.__get_pagination_filters(request)
+                return {**cm_flt, **tm_flt, **pg_flt}
+
+            elif request_param == 'POST':
+                pass
+
+        return filter_method
+
+    @staticmethod
+    def __get_common_filters(self, request) -> dict[str, str]:
+        return dict(
+            map(
+                lambda common_filter_list: (
+                    (self.__join_to if self.__join_to else '') + common_filter_list[0], common_filter_list[-1]
+                ),
+                filter(
+                    lambda value: value is not None,
+                    (
+                        self.get_filter_list_or_none(request, filter_name)
+                        for filter_name in HttpRequestFilter.__common_filter_names
+                    )
+                )
+            )
+        )
+
+    @staticmethod
+    def __get_timed_filters(self, request) -> dict[str, str]:
+
+        timed_filters = dict()
+        for timed_filter_name in HttpRequestFilter.__timed_filter_names:
+            if timed_filter_list := self.get_filter_list_or_none(request, timed_filter_name):
+                operator = '__gte' if timed_filter_name == 'start' else '__lte'
+                timed_filters[
+                    f'{self.__join_to if self.__join_to else ""}created_at{operator}'
+                ] = timed_filter_list[-1]
+
+        return timed_filters
+
+    @staticmethod
+    def __get_pagination_filters(request) -> dict[str, int]:
+
+        page = abs(int(request.GET.get('page', 1)))
+        page_size = min(abs(int(request.GET.get('page_size', 20))), 20)
+
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+
+        return {'page': start_index, 'page_size': end_index}
+
+
+class HttpRequestFilterABC(abc.ABC):
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_filter_list_or_none(request, filter_name: str) -> Optional[list[str, str]]:
+        """ Based on filter name availability inside one of HTTP method
+        namespace returns collection of filter name and its value. Otherwise, returns None. """
+
+    @abc.abstractmethod
+    def get_filters(self):
+        """ Returns dictionary with all filters from Request. """
+
+
+class ReqFilter(metaclass=HttpRequestFilter):
+
+    __available_request_params = ('GET', 'POST')
+
+    def __init__(self, request_param: str, *, request, join_to=None):
+
+        assert request_param in self.__available_request_params, (
+                'Unavailable request parameter. Use %s' % ', '.join(self.__available_request_params)
+        )
+        assert isinstance(join_to, (str | None)), 'Type of join_to parameter must be either str or None.'
+
+        self.__req_param = request_param
+        self.__req = request
+        self.__join_to = join_to
+
+    @staticmethod
+    def get_filter_list_or_none(request, filter_name: str) -> Optional[list[str]]:
+
+        if filter_value := request.GET.get(filter_name):
+            return [filter_name, filter_value]
+        return None
+
+    def get_filters(self):
+        filter_method = getattr(self, f'{self.__req_param.lower()}_filter', None)
+        if filter_method:
+            return filter_method(self.__req)
+        return {}
--- filemanager/api/views.py
+++ filemanager/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 
 from .serializers import FileSerializer
 from .models import UserFile
+from .utils import ReqFilter
 
 
 class ShowUserFilesDetail(APIView):
@@ -18,43 +19,15 @@ class ShowUserFilesDetail(APIView):
         if user_id is None:
             raise Http404
 
-        common_filter_names = (
-            'file_extension',
-            'file_uuid',
-            'status',
-        )
+        req_f = ReqFilter('GET', request=request, join_to='file_id__')
 
-        common_filters = dict(
-            map(
-                lambda common_filter_list: ('file_id__' + common_filter_list[0], common_filter_list[-1]),
-                filter(
-                    lambda value: value is not None,
-                    (self.get_not_none_filter(request, filter_name) for filter_name in common_filter_names)
-                )
-            )
-        )
-
-        timed_filter_names = (
-            'start',
-            'end',
-        )
-
-        timed_filters = dict()
-        for timed_filter_name in timed_filter_names:
-            if timed_filter_list := self.get_not_none_filter(request, timed_filter_name):
-                operator = '__gte' if timed_filter_name == 'start' else '__lte'
-                timed_filters[f'file_id__created_at{operator}'] = timed_filter_list[-1]
-
-        page = int(request.GET.get('page', 1))
-        page_size = min(abs(int(request.GET.get('page_size', 20))), 20)
-
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
+        request_filters = req_f.get_filters()
+        start_index = request_filters.pop('page')
+        end_index = request_filters.pop('page_size')
 
         user_files = UserFile.objects.filter(
             user_id=user_id,
-            **common_filters,
-            **timed_filters,
+            **request_filters,
         )[start_index:end_index]
 
         serializer = FileSerializer(user_files, many=True)
@@ -63,10 +36,3 @@ class ShowUserFilesDetail(APIView):
             return Response({'user_id': user_id, 'files': serializer.data})
         except UserFile.DoesNotExist:
             raise Http404
-
-    @staticmethod
-    def get_not_none_filter(request, filter_name: str) -> Optional[List]:
-
-        if filter_value := request.GET.get(filter_name):
-            return [filter_name, filter_value]
-        return None
