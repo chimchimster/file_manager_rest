08e556503880452a374ca51bbe94538c974323e9
chimchimster <jorddyishere@gmail.com>
2024-01-19 16:01:21 +0600

    add static

---
+++ filemanager/static/js/download_file.js
@@ -0,0 +1,9 @@
+function downloadFile(fileData, fileName) {
+		const blob = new Blob([fileData], { type: 'application/octet-stream' });
+		const link = document.createElement('a');
+		link.href = URL.createObjectURL(blob);
+		link.download = fileName;
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}
