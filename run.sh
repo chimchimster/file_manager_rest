8436f2ea4a36da4276bdbd48de21ad089274059c
chimchimster <jorddyishere@gmail.com>
2024-01-22 10:09:29 +0600

    changed logs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd filemanager && gunicorn --workers=7 filemanager.wsgi --bind 0.0.0.0:8000 & cd filemanager && celery -A filemanager worker --loglevel=info
+cd filemanager && celery -A filemanager worker --loglevel=info & cd filemanager && gunicorn --workers=7 filemanager.wsgi --bind 0.0.0.0:8000
