3112a3aaa6b89498c7d4cb25068fac566e9132d3
chimchimster <jorddyishere@gmail.com>
2024-01-22 10:54:24 +0600

    bugfixes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd filemanager && celery -A filemanager worker --loglevel=info & cd filemanager && gunicorn --workers=7 filemanager.wsgi --access-logfile '-' --logfile '-' --bind 0.0.0.0:8000
+cd filemanager && celery -A filemanager worker --loglevel=info & cd filemanager && gunicorn --workers=7 filemanager.wsgi --access-logfile '-' --bind 0.0.0.0:8000
